e4543ff34af9c1c33cc606393e6fd41f945ccc4a
Arman <armanmahal2004@gmail.com>
2024-03-25 09:38:05 +0530

    modified:   L17/25_implementation_of_bubble_sort.cpp

--- L17/25_implementation_of_bubble_sort.cpp
+++ L17/25_implementation_of_bubble_sort.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void bubbleSort(int arr[], int size)
 {
     //traversal of swapping will happen for size-1 times
-    for (int i = 0; i < size - 1; i++)
+    for (int i = 1; i < size ; i++)
     {
 
         int j = 0;
