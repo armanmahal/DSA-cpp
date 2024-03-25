0a1036d5784bd5dafd18725efb6a2d97eaf3f245
Arman <armanmahal2004@gmail.com>
2024-03-25 09:25:15 +0530

    commit

---
+++ L17/25_implementation_of_bubble_sort.cpp
@@ -0,0 +1,48 @@
+// sort a given array with bubble sort
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int size)
+{
+    //traversal of swapping will happen for size-1 times
+    for (int i = 0; i < size - 1; i++)
+    {
+
+        int j = 0;
+        bool ifSwapped = false;
+
+        while (j < (size - i))
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                ifSwapped = true;
+            }
+            j++;
+        }
+        //if no swaps happen, it means array is already sort, then just break the loop:
+        if (ifSwapped == false)
+        {
+            break;
+        }
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    int arr[10] = {3, 23, 564, 12, 8, 23, 4, 1, 0, 12};
+
+    bubbleSort(arr, 10);
+
+    printArray(arr, 10);
+}
