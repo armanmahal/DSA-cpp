db23cf1b36f94451a1aa0f8a73201b8b03694d2a
Arman <armanmahal2004@gmail.com>
2024-06-06 19:47:45 +0530

    L56

---
+++ L56/101_largest_rectangular_area_in_histogram.cpp
@@ -0,0 +1,9 @@
+//finding the largest reactangular area in histogram.
+//statement:
+/*
+https://www.geeksforgeeks.org/largest-rectangular-area-in-a-histogram-using-stack/
+*/
+//solutions:
+/*
+https://takeuforward.org/data-structure/area-of-largest-rectangle-in-histogram/
+*/
