b49a1d639956aa778ef61c31c5d9a36b19b3b3d0
Arman <armanmahal2004@gmail.com>
2024-06-06 20:14:23 +0530

    L58

---
+++ L58/103_'n'_stacks_in_a_array.cpp
@@ -0,0 +1,12 @@
+/*
+Problem Statement:  Design a data structure to implement ‘N’ stacks using a single array of size ‘S’. It should support the following operations:
+
+push(X, M): Pushes an element X into the Mth stack. Returns true if the element is pushed into the stack, otherwise false.
+
+pop(M): Pops the top element from Mth Stack. Returns -1 if the stack is empty, otherwise, returns the popped element.
+*/
+
+//APPROACH:
+/*
+https://takeuforward.org/data-structure/implement-k-stacks-in-a-single-array/
+*/
